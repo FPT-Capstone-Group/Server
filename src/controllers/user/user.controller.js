89746feb1c01af69079498e075a5f24a6421d082
Tuan Nguyen <tuan.nguyen@us.gooddata.com>
2023-12-09 07:46:36 +0700

    update user controller

--- src/controllers/user/user.controller.js
+++ src/controllers/user/user.controller.js
@@ -96,12 +96,13 @@ const login = async (req, res) => {
     if (!isPasswordValid) {
       throw new Error("Incorrect username Id/Password");
     }
-    // Check if the user has a firebaseToken field, if user don't have, skip it
-    if (user.hasOwnProperty("firebaseToken")) {
+      // Check if the user has a firebaseToken field, if user don't have, skip it
       // Update the user's firebaseToken
       user.firebaseToken = req.body.firebaseToken;
+      console.log(`TOKEN: ${req.body.firebaseToken}`)
       await user.save();
-    }
+      console.log(`SAVED user: ${user}`)
+
     const token = jwt.sign(
       {
         user: {
@@ -265,7 +266,8 @@ const forgotPassword = async (req, res) => {
       throw new Error("User not found");
     }
     const verificationCheckStatus = await verifyOtpToken(username, otpToken)
-    if (!verificationCheckStatus.localeCompare("approved")){
+    if (verificationCheckStatus.localeCompare("approved") !== 0){
+      console.error("Please provide valid OTP Token!")
       throw new Error("Please provide valid OTP Token!");
     }
     // Update the user's password
