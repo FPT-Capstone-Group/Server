85bbc886d159ad107ab9a2fa94c7f79e777da6d6
Tuan Nguyen <51945324+tuannnh@users.noreply.github.com>
2024-01-09 10:40:21 +0700

    Update adminAuth.js

--- src/middleware/adminAuth.js
+++ src/middleware/adminAuth.js
@@ -7,7 +7,7 @@ const adminAuth = async (req, res, next) => {
 
     // Check if the user has the 'Admin' role directly
     const isAdmin = await Role.findOne({
-      where: { name: "admin" },
+      where: { roleName: "admin" },
       include: [
         {
           model: User,
